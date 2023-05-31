ef996b0c04d0fc7831b0965b55380c2d91b59585
dare <rosascript@gmail.com>
2023-05-31 04:04:33 -0400

    [add] fisher install script to {fish} config

---
+++ dot_config/private_fish/run_onchange_fisher_install.sh
@@ -0,0 +1,15 @@
+#!/bin/fish
+
+if not test -e "$HOME/.config/fish/functions/fisher.fish"
+  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+end
+
+fisher install jhillyerd/plugin-git
+fisher install jethrokuan/fzf
+fisher install laughedelic/pisces
+fisher install jorgebucaran/fish-bax
+fisher install danhper/fish-ssh-agent
+fisher install oh-my-fish/plugin-license
+fisher install halostatue/fish-docker@v1.x
+fisher install oh-my-fish/plugin-gi
+fisher install jorgebucaran/autopair.fish
